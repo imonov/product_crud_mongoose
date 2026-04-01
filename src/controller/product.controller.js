2f1748b4d86bf1faf3e8420f08cd268aca4afa55
Anvar <anvar@imonov.uz>
2026-04-02 00:29:04 +0500

    update method added

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -36,3 +36,37 @@ export async function createProduct(req, res) {
         message: "product yaratildi",
     });
 }
+
+export async function updateProduct(req, res) {
+    const { id } = req.params;
+
+    const { name, description, price, quantity, rating, manufacture_year } =
+        req.body;
+
+    const updatedData = {};
+
+    if (name) updatedData.name = name;
+    if (description) updatedData.description = description;
+    if (price) updatedData.price = price;
+    if (quantity) updatedData.quantity = quantity;
+    if (rating) updatedData.rating = rating;
+    if (manufacture_year) updatedData.manufacture_year = manufacture_year;
+
+    const updatedProduct = await Product.findByIdAndUpdate(id, updatedData, {
+        new: true,
+        runValidadors: true,
+    });
+
+    if (!updatedProduct) {
+        res.status(404).json({
+            status: 201,
+            message: "mahsulot topilmadi",
+        });
+    }
+
+    res.status(200).json({
+        status: 200,
+        message: "mahsulot yangilandi",
+        data: updatedProduct,
+    });
+}
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import {
     createProduct,
     getAllProduct,
+    updateProduct,
 } from "../controller/product.controller.js";
 
 export const productRouter = Router();
-productRouter.get("/products", getAllProduct).post("/products", createProduct);
+productRouter
+    .get("/products", getAllProduct)
+    .post("/products", createProduct)
+    .put("/products/:id", updateProduct);
